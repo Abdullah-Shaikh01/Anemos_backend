026c3369034165b078e9b5a45acc1d14323cdd8c
MaStErMiNd6001 <you@example.com>
2022-10-01 16:11:10 +0530

    remove unnecessary requires.

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const { con, auth } = require("../config/dbconfig");
+const { con } = require("../config/dbconfig");
 const { encrypt, match } = require("../util/encrypt.js");
 const refreshToken = require("../controllers/refreshToken");
 const router = express.Router();
